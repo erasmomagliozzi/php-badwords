8b80bb68c35504fd4806a3f7509546d86954c1e2
Erasmo Magliozzi <magliozzierasmo98@gmail.com>
2020-02-20 17:53:45 +0100

    coomit

--- script.php
+++ script.php
@@ -3,7 +3,7 @@ Visualizzare a schermo il paragrafo con la relative lunghezza e sostituire la ba
 <?php
   $paragraph = 'Lorem ipsum dolor sit Lorem, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.';
 
-  $badword = $_GET["badword"];
+  $badword = $_GET["parola"];
  ?>
 
  <!DOCTYPE html>
